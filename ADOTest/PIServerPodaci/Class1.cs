0e7a0a8a011c492424dbdf8ea4ff921bde96932f
Domagoj Lunder <domagojdomy3@gmail.com>
2022-06-15 18:56:32 +0200

    First version

---
+++ ADOTest/PIServerPodaci/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIServerPodaci
+{
+    public static class Class1
+    {
+        public static List<Prijava> GetPrijava()
+        {
+            using (var konteks = new PI2221_DBEntities())
+            {
+                var upit = from u in konteks.Prijavas
+                           select u;
+                return upit.ToList();
+            }
+
+        }
+    }
+}
